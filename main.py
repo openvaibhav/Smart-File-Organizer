a1b009bc9f6c9b047850c4fae5bb1fdcd52941a5
Vaibhav Kumar <59310383+openvaibhav@users.noreply.github.com>
2026-02-14 12:22:32 +0530

    Recursive and Exclusion added

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+# Imports
 from pathlib import Path
 import os
 import shutil
@@ -6,25 +7,28 @@ import argparse
 import sys
 import json
 
-#Argparse setup
+# Argparse setup
 parser = argparse.ArgumentParser(description="Automate everyday file management tasks")
 parser.add_argument('-s','--src',metavar='src',type=str,help='Input Source of where you want the automation to be done')
 parser.add_argument('-d','--des',metavar='des',type=str,help='Input Destination of where you want the final output')
-parser.add_argument('-m','--mode',metavar='mode',type=str,help='Mode of Script - Copy | Move') #for now just files
+parser.add_argument('-m','--mode',metavar='mode',type=str,help='Mode of Script - Copy | Move')
+parser.add_argument('-r', '--recursive', action='store_true',help='This will sort all files and even files inside subfolders')
+parser.add_argument('-e', '--exclude', metavar='exclude',help='This will exclude files and folders (Separate inputs with commas)')
 parser.add_argument('-dr', '--dry_run', action='store_true',help='Dry Run Preview (No Execution)')
+
 args = parser.parse_args()
 
-#Variables and lists
-file_records = []
+# Variables and lists
 files = []
-folders = []
-unk_items = []
 src = args.src
 des = args.des
 mode = args.mode.lower() if args.mode else None
+if args.exclude:
+    exclude_list = set(args.exclude.split(","))
+else:
+    exclude_list = set()
 
-
-#Extensions load from json
+# Extensions load from json
 with open("extensions.json", "r") as f:
     data = json.load(f)
 
@@ -35,7 +39,7 @@ video_ext_map = data["video_ext_map"]
 archive_ext_map = data["archive_ext_map"]
 categories = data["categories"]
 
-#Source initialisation
+# Source initialisation
 def source_cur_dir():
     p = Path(".")
     return p
@@ -47,7 +51,7 @@ def source_other_dir(src):
     else:
         raise Exception("Path does not exist")
 
-#Destination initialisation
+# Destination initialisation
 def dest_cur_dir():
     d = Path(".")
     return d
@@ -59,12 +63,34 @@ def dest_other_dir(des):
     else:
         raise Exception("Path does not exist")
     
-#Copy or Move Files Function
+# Files Iteration
+def collect_files(P,exclude_list):
+    forbidden = set(exclude_list)
+    for x in P:
+        if forbidden and any(part in forbidden for part in x.parts):
+            continue
+        
+        if x.is_file():
+            stat = x.stat()
+            a = dict(
+                file=x,
+                size=stat.st_size,
+                cr_time=datetime.datetime.fromtimestamp(stat.st_ctime),
+                mod_time=datetime.datetime.fromtimestamp(stat.st_mtime),
+                cat="",
+                sub_cat=""
+            )
+            files.append(a)
+    
+# Copy or Move Files Function
 def copy_move_files(mode, des, files):
     t = 0
+    c = set()
     for x in files:
         file = x['file']
         loc = f"__{x['cat']}" if x['cat'] else ""
+        if loc not in c:
+            c.add(loc)
         try:
             src_path = str(file)
             dest_dir = os.path.join(des, loc, x.get('sub_cat', ''))
@@ -82,9 +108,10 @@ def copy_move_files(mode, des, files):
             t += 1
         except Exception as e:
             raise(e)
-    print(f"Sorted({strng}) {t} files into to {des}")
+    cc = len(c)
+    print(f"Sorted({strng}) {t} files into {cc} Categories to {des}")
 
-#Dry Run Preview
+# Dry Run Preview
 def dry_run_preview(des, files):
     prepped_paths = []
     for x in files:
@@ -106,7 +133,7 @@ def dry_run_preview(des, files):
     for srcc, destt in prepped_paths:
         print(f"{srcc:<{max_src_len}} --> {destt}")
 
-#Variable locations setup
+# Variable locations setup
 if src == None:
     p = source_cur_dir()
 elif src != None:
@@ -117,33 +144,11 @@ if des == None:
 elif des != None:
     d = dest_other_dir(des)
 
-#Collecting Metadata
-for x in p.iterdir():
-    stat = x.stat()
-    a = dict(
-        file=x,
-        size=stat.st_size,
-        cr_time=datetime.datetime.fromtimestamp(stat.st_ctime),
-        mod_time=datetime.datetime.fromtimestamp(stat.st_mtime),
-        type="",
-        cat="",
-        sub_cat=""
-    )
-    file_records.append(a)
+# Collecting Metadata
+P = p.rglob("*") if args.recursive else p.iterdir()
+collect_files(P, exclude_list)
 
-#Distinguishing Files/Folders/Unk
-for x in file_records:
-    if x["file"].is_file():
-        x.update(type='file')
-        files.append(x)
-    elif x["file"].is_dir():
-        x.update(type='folder')
-        folders.append(x)
-    else:
-        x.update(type='unknown')
-        unk_items.append(x)
-
-#Updating Category and Sub-Category of Files
+# Updating Category and Sub-Category of Files
 for x in files:
     ext = str(x['file'].suffix).lower()
     if ext in doc_ext_map:
@@ -159,7 +164,7 @@ for x in files:
     else:
         x.update(cat="Uncategorized")
         
-#Main CLI
+# Main CLI
 if len(sys.argv) == 1:
     print(f"{parser.prog}: try 'python {parser.prog} --help' for more information")
     sys.exit(1)
